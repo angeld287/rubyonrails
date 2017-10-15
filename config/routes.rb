e74d8a8c7d5c9fafa21852f9befc32cd38feaccb
Angel Angeles <daniel_1234@hotmail.es>
2017-10-15 11:45:21 -0400

    tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'propiedades/index'
 
   resources :propiedades
 
-  root 'welcome#index'
+  root 'propiedades#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
