bc0e3f2d0819ac7e9696bfb9263bcd04ef84545a
Angel Angeles <daniel_1234@hotmail.es>
2017-10-15 21:22:30 -0400

    cuarto commit

--- app/models/propiedade.rb
+++ app/models/propiedade.rb
@@ -1,4 +1,8 @@
 class Propiedade < ApplicationRecord
 	validates :descripcion, presence: true,
                     length: { minimum: 5 }
+    validates :valoracion, presence: true, numericality: true
+    validates :direccion, presence: true,
+                    length: { minimum: 10 }
+    validates :dimensiones, presence: true, numericality: true
 end
